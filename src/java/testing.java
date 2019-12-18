b83d0cf9f6626b58be90031ca682b42c9e44871f
Robin8057 <navitus.robin@gmail.com>
2019-12-18 11:12:47 +0530

    testing

--- src/java/testing.java
+++ src/java/testing.java
@@ -12,9 +12,15 @@ public class testing {
     
    public void testing(){
    
+<<<<<<< HEAD
        System.out.println("testing ....... enneneneneeneneneneneenenenennen");
        
        
+=======
+       System.out.println("testing ........");
+       
+        System.out.println("testing ........");
+>>>>>>> origin/testing1
    }
     
 }
